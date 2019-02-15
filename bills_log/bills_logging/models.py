bdd50446d5434ca0b40e74e985aec2474eba7704
ivan <ialeksandrov1990@abv.bg>
2019-02-15 17:42:32 +0200

    Create a basic database, some timezone modifications and test in Django shell

--- bills_log/bills_logging/models.py
+++ bills_log/bills_logging/models.py
@@ -4,3 +4,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Bills(models.Model):
+    bill_name = models.CharField(max_length=200)
+    bill_month = models.CharField(max_length=50)
+    bill_amount = models.DecimalField(decimal_places=2, max_digits=1000)
+
+    def __str__(self):
+        return self.bill_name
